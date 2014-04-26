9cba6fe4896c10b7f2cbbdb9c650c02f54082509
Chris Prater <cprater23@gmail.com>
2014-04-25 22:07:40 -0500

    almost displaying patterns. messing with setinterval. its late. im out

--- app/assets/javascripts/simon_says.js
+++ app/assets/javascripts/simon_says.js
@@ -1,21 +1,48 @@
 var Game = function(board){
 	var gameBoard = board;
-	var move = 1;
+	var move = 0;
+	positions = [];
 
 	function randomInt(){
 		return Math.floor(Math.random() * (8 - 0 + 1)) + 0;
 	}
 
 	function showPiece(index){
-		_this = gameBoard[index];
-		color = $(_this).attr('id');
-		gameBoard[index].style.background = $(_this).css('background', color);
+		console.log("show");
+		var _this = gameBoard[index];
+		var color = $(_this).attr('id');
+		$(_this).css('background', color);
 	}
 
+	function hidePiece(index){
+		console.log("hide");
+		var _this = gameBoard[index];
+		var color = $('.game-board').css('background-color');
+		// setTimeout(function(){
+			$(_this).css('background-color', color);
+		// }, 1000);
+	}
+
+	function displayPattern(index){
+		// var i = 0;
+		// if (i++ < pattern.length){
+			showPiece(positions[index]);
+			setTimeout(function(){
+				hidePiece(positions[index]);
+			}, 1000);
+		// }
+
+	}
+	
 	this.play = function(){
-		showPiece(randomInt());
+		positions.push(randomInt());
+		for(i =0; i < positions.length; i++){
+			displayPattern(i);
+		}
+		
 	};
 
+	
 };
 
 
@@ -27,6 +54,7 @@ $(function(){
 		e.preventDefault();
 
 		game.play();
-
+		
 	});
+
 });
